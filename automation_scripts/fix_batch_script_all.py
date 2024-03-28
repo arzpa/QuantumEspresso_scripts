2f0f812f39654c2102d893be3f68093f891c10a8
parastooaghr <parastoo.agharezaei@gmail.com>
2024-03-28 15:58:33 -0400

    Update fix_batch_script_all.py
    Removed the name of materials

--- automation_scripts/fix_batch_script_all.py
+++ automation_scripts/fix_batch_script_all.py
@@ -1,11 +1,10 @@
 import os
 import shutil
 import re
-Subhmoy_materials_str = 'Co3Ni  CoPt3  CuPt   Fe3Co   FeCo  FeNi3  FePt3  NiPt VPt   VPt3  Zn11Co2  Zn13Co  Zn22Ni3 Zn3Cu  Zn8Cu5  ZnCu  ZnPt Co3Pt  Cu3Pt  CuPt7  Fe9Co7  FeNi  FePt   Ni3Pt  NiPt3  V3Pt VPt2  VPt8  Zn11Ni2  Zn13Fe  Zn35Cu17  Zn3Pt  Zn9Fe4  ZnNi  ZnPt3'
+Subhmoy_materials_str = ''
 Subhmoy_materials = Subhmoy_materials_str.split()
 
-Satvik_materials_str = 'Al13Fe4  Al3Ni   Al3Pt5  Al5Co2  AlCu3  AlNi3  AlV   V3Ni   VFe3 Al21Pt8  Al3Ni2  Al3V    Al9Co2   AlFe   AlPt   AlV3  V3Ni2  VNi2 Al2Cu    Al3Ni5  Al4Cu9  AlCo    AlFe3  AlPt2  V3Co  V4Zn5  VNi3 Al2Pt    Al3Pt2  Al4Ni3  AlCu    AlNi   AlPt3  V3Fe  VCo3   VZn3'
-
+Satvik_materials_str = ''
 Satvik_materials = Satvik_materials_str.split()
 
 person ='Satvik'
